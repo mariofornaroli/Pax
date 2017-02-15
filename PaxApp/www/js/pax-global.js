806a7a2c9360f3230d604064d23d59ee8215ca4f
fornaroli <fornaroli@PC1545>
2017-02-15 14:01:54 +0100

    connect to cloud services

--- PaxApp/www/js/pax-global.js
+++ PaxApp/www/js/pax-global.js
@@ -2,8 +2,9 @@
 
 /* ---- App url ---- */
 //paxGlobal.appUrl = '';
-paxGlobal.appUrl = 'http://localhost:51267/'; 
-//paxGlobal.appUrl = 'http://paxwebapi.azurewebsites.net/';
+//paxGlobal.appUrl = 'http://localhost:51267/';
+//paxGlobal.appUrl = 'http://localhost:8091/';
+paxGlobal.appUrl = 'http://paxwebapi.azurewebsites.net/';
 
 paxGlobal.getAppUrl = function getAppUrl() {
     if (paxGlobal.appUrl) {
